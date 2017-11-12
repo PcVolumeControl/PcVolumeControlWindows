ea49ce2f1ac96b4dc9bfdcb1551e0c2334695eb5
Wavesonics <adamwbrown@gmail.com>
2017-11-11 16:04:00 -0800

    Added icons, cleaned up the window a little

--- VolumeControl/Properties/Resources.Designer.cs
+++ VolumeControl/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace VolumeControl.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Application {
+            get {
+                object obj = ResourceManager.GetObject("Application", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap WindowIcon {
+            get {
+                object obj = ResourceManager.GetObject("WindowIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
